218ab8b16672e4e88370ca35ab186b10627b43fd
AnjanyKumarJaiswal <136046942+AnjanyKumarJaiswal@users.noreply.github.com>
2024-01-12 20:20:53 +0530

    integrated angular with custom html-css file

--- greenwheels/greenwheels/src/app/app.component.ts
+++ greenwheels/greenwheels/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './styles.css'
 })
 export class AppComponent {
   title = 'greenwheels';
