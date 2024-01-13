964f274fd366fe40f264a2bdbda894685ee39ae8
AnjanyKumarJaiswal <136046942+AnjanyKumarJaiswal@users.noreply.github.com>
2024-01-13 14:39:31 +0530

    added hamburg menu

--- greenwheels/greenwheels/src/app/app.component.ts
+++ greenwheels/greenwheels/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HomepageComponent } from './homepage/homepage.component';
+
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet ,HomepageComponent]
+    imports: [CommonModule, RouterOutlet ]
 })
 export class AppComponent {
   title = 'greenwheels';
