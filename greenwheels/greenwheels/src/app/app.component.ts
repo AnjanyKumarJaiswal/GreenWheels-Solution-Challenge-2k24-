87772997291f8fdb761bdc96c51c4ab25e389d00
AnjanyKumarJaiswal <136046942+AnjanyKumarJaiswal@users.noreply.github.com>
2024-01-13 10:25:38 +0530

    dataset added and few changes done

--- greenwheels/greenwheels/src/app/app.component.ts
+++ greenwheels/greenwheels/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { HomepageComponent } from './homepage/homepage.component';
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './styles.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet ,HomepageComponent]
 })
 export class AppComponent {
   title = 'greenwheels';
