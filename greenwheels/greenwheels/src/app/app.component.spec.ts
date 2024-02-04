3f591b8acd2f1f2b3b8af28d25a50c68a8981e75
AnjanyKumarJaiswal <136046942+AnjanyKumarJaiswal@users.noreply.github.com>
2024-02-04 11:11:52 +0530

    done

--- greenwheels/greenwheels/src/app/app.component.spec.ts
+++ greenwheels/greenwheels/src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
   });
 
@@ -14,16 +15,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'greenwheels' title`, () => {
+  it(`should have the 'GreenWheels' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('greenwheels');
+    fixture.detectChanges(); // Ensure the component has been rendered
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('GreenWheels');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, greenwheels');
+    expect(compiled.querySelector('h1')?.textContent).toContain('GreenWheels');
   });
 });
--- greenwheels/greenwheels/src/app/app.component.ts
+++ greenwheels/greenwheels/src/app/app.component.ts
@@ -3,15 +3,28 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
+import { Router   } from '@angular/router';
+import { NavigationEnd } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
+    styleUrls: ['./app.component.css'],
     imports: [CommonModule, RouterOutlet  , RouterLink , RouterLinkActive]
 })
 export class AppComponent {
-  title = 'greenwheels';
+  title = 'GreenWheels';
+  showGreenWheelsInfo: boolean = true;
+  showGreenWheelsInfo1: boolean = true;
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Logic to determine which components should show/hide the divs
+        this.showGreenWheelsInfo = !event.url.includes('/vehicle-recommender') && !event.url.includes('/emission-map');
+        this.showGreenWheelsInfo1 = !event.url.includes('/emission-map') && !event.url.includes('/vehicle-recommender');
+      }
+    });
+  }
 }
 
