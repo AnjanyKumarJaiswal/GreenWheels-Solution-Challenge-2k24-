0adea6b824e8f335230fbc7b349a9c0aededac8a
AnjanyKumarJaiswal <136046942+AnjanyKumarJaiswal@users.noreply.github.com>
2024-02-06 20:45:03 +0530

    done

---
+++ greenwheels/greenwheels/src/app/vehicle-recommender/vehicle-recommmender.js
@@ -0,0 +1,8 @@
+
+const chatcontainer = document.querySelector("chatting");
+const sendprompt = document.getElementById("sendprompt");
+const userinput = document.getElementById("userinput");
+
+
+
+
